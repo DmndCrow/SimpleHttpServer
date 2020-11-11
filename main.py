2c6aa3d99cb4eccd9ca8da1abda90216154c3763
aibek <aibek.ziyashev@nu.edu.kz>
2020-11-11 18:40:54 +0600

    [ADD] get routes / and /hello

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 import os
-import http.server
-import socketserver
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
 PORT = int(os.getenv('HTTP_PORT', '3000'))
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(('', PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+
+class HandleRoute:
+    def __init__(self):
+        self.routes = {
+            '/': self.root(),
+            '/hello': self.hello()
+        }
+
+    def root(self):
+        return 'Root Directory'
+
+    def hello(self):
+        return 'Hello World'
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json; charset=utf-8')
+        self.end_headers()
+
+        routes = HandleRoute().routes
+
+        if self.path in routes:
+            data = {
+                'response': routes[self.path]
+            }
+        else:
+            data = {
+                'response': 'oops'
+            }
+
+        self.wfile.write(json.dumps(data).encode())
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('localhost', PORT), Handler)
+
+    try:
+        print('Server is running at port', PORT)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
